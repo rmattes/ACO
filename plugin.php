8b3fd18076c36344911e993bae2dde2497782ad4
Kai Durst <kai.durst@gmx.de>
2017-03-16 23:33:19 +0100

    Plugin Info ergänzt

--- plugin.php
+++ plugin.php
@@ -4,5 +4,5 @@ $version = '0.9.0';
 $ilias_min_version = '5.2.0';
 $ilias_max_version = '5.2.999';
 $responsible = 'Manuel Mergl / Kai Durst / Philipp Gruber';
-$responsible_mail = 'merglml@studi.informatik.uni-stuttgart.de /  / gruberpp@studi.informatik.uni-stuttgart.de';
+$responsible_mail = 'merglml@studi.informatik.uni-stuttgart.de / durstrl@studi.informatik.uni-stuttgart.de / gruberpp@studi.informatik.uni-stuttgart.de';
 ?>

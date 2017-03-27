c9b88698e91cf059ab0384ae32f4b73367a92ead
Jan Ruthardt <janruthardt@web.de>
2017-03-27 13:28:38 +0200

    Minimalversion runtergesetzt

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 $id = 'acop';
 $version = '0.9.0';
-$ilias_min_version = '5.2.0';
+$ilias_min_version = '5.0.0';
 $ilias_max_version = '5.2.999';
 $responsible = 'Manuel Mergl / Kai Durst / Philipp Gruber';
 $responsible_mail = 'merglml@studi.informatik.uni-stuttgart.de / durstrl@studi.informatik.uni-stuttgart.de / gruberpp@studi.informatik.uni-stuttgart.de';

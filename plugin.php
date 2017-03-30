951661615f65b730abd8f81578e99cf255e1463a
Kai Durst <kai.durst@gmx.de>
2017-03-30 21:45:26 +0200

    Version auf 1.0 gesetzt

--- plugin.php
+++ plugin.php
@@ -1,6 +1,6 @@
 <?php
 $id = 'acop';
-$version = '0.9.0';
+$version = '1.0.0';
 $ilias_min_version = '5.2.0';
 $ilias_max_version = '5.2.999';
 $responsible = 'Manuel Mergl / Kai Durst / Philipp Gruber';
